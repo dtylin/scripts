40a25d42e1be00debbc59ec0616ffa19439fda21
Dmitriy Tylin <d_tylin@yahoo.com>
2020-08-20 15:28:31 +0300

    Update sendmail.php

--- form/sendmail.php
+++ form/sendmail.php
@@ -1,4 +1,5 @@
 <?php
+if ((isset($_POST['name'])) && (isset($_POST['tel'])) &&) {
     $name = $_POST['name'];
     $tel = $_POST['tel'];
     $headers= "MIME-Version: 1.0\r\n";
@@ -7,4 +8,8 @@
     $subHeader = "Новая заявка на обогреватель с лендинга: ".$name;
     $text = '<strong>Телефон: </strong>'.$tel.'<br>';
     mail('test1@gmail.com, test2@mail.ru', $subHeader, $text, $headers);
+    echo json_encode(array('success' => 1));
+  } else {
+    echo json_encode(array('success' => 0));
+  }
 ?>
