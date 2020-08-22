cd6047caf8bc6fda64bde8fa6e099b3e4bf5a5cb
Dmitriy Tylin <d_tylin@yahoo.com>
2020-08-22 17:50:59 +0300

    Update form.js

--- Обработка лидов/form/form.js
+++ Обработка лидов/form/form.js
@@ -10,8 +10,6 @@ $('form').submit(function () {
     },
     success: function (response) {
       var jsonData = JSON.parse(response);
-      console.log(jsonData);
-      console.log(jsonData.success)
       if (jsonData.success == '1'){
         alert('Сообщение отправлено');
       } else if (jsonData.success == '0') {
